06f25f23d6d33266b84bd42e9dd3e4f38c537876
GArnold02 <garnold02@gmail.com>
2021-10-05 19:12:13 +0200

    Launch app from entry point

--- viewport/entry/src/lib.rs
+++ viewport/entry/src/lib.rs
@@ -1,3 +1,5 @@
+use logic::Viewport;
+use tools::app::App;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -5,4 +7,8 @@ pub fn main() {
     run();
 }
 
-fn run() {}
+fn run() {
+    let app = App::default();
+    let viewport = Viewport::default();
+    app.run(viewport);
+}
