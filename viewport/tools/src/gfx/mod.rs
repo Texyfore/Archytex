7b056487610689e18b8e34498e3fcd64435a17e5
GArnold02 <garnold02@gmail.com>
2021-10-07 18:54:59 +0200

    Rolled back WebGL abstractions

--- viewport/tools/src/gfx/mod.rs
+++ viewport/tools/src/gfx/mod.rs
@@ -1,36 +1,11 @@
-mod context;
-mod mesh;
-
-use context::Context;
-use glow::*;
-use std::rc::Rc;
-use web_sys::HtmlCanvasElement;
-
-pub struct Graphics {
-    context: Rc<Context>,
-}
+pub struct Graphics;
 
 impl Graphics {
-    pub fn new(canvas: &HtmlCanvasElement) -> Self {
-        let context = Context::new(canvas);
-
-        unsafe {
-            context.gl.enable(DEPTH_TEST);
-            context.gl.clear_color(0.25, 0.25, 0.25, 1.0);
-        }
-
-        Self { context }
+    pub fn new() -> Self {
+        Self
     }
 
-    pub fn resize_viewport(&self, width: i32, height: i32) {
-        unsafe {
-            self.context.gl.viewport(0, 0, width, height);
-        }
-    }
+    pub fn resize_viewport(&self, width: i32, height: i32) {}
 
-    pub fn begin(&self) {
-        unsafe {
-            self.context.gl.clear(COLOR_BUFFER_BIT | DEPTH_BUFFER_BIT);
-        }
-    }
+    pub fn begin(&self) {}
 }
