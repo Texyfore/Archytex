db337415c2f49fdfdf89f0cf5eb3f3f10ce5d00e
GArnold02 <garnold02@gmail.com>
2021-10-07 22:56:45 +0200

    Added console.log()

---
+++ viewport/tools/src/log.rs
@@ -0,0 +1,14 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(msg: &str);
+}
+
+#[macro_export]
+macro_rules! console {
+    ($($t:tt)*) => {
+        tools::log::log(&format_args!($($t)*).to_string())
+    };
+}
