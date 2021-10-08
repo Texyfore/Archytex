92384998efaaf199f6118a9759235f477870e6b3
GArnold02 <gulyas.arnold02@gmail.com>
2021-10-08 12:59:23 +0200

    Added input event types

--- viewport/tools/src/app/event.rs
+++ viewport/tools/src/app/event.rs
@@ -1,3 +1,26 @@
 pub enum Event {
-    Initialized
+    Initialized,
+    RawInput(RawInputKind),
+}
+
+pub enum RawInputKind {
+    Key(InputState, KeyKind),
+    Button(InputState, ButtonKind),
+    Movement(f32, f32),
+}
+
+pub enum InputState {
+    Pressed,
+    Released,
+}
+
+pub enum KeyKind {
+    Unknown,
+}
+
+pub enum ButtonKind {
+    Left,
+    Right,
+    Middle,
+    Unknown,
 }
