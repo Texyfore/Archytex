8ecb40f23f2423ac5148251aaa76068d767fc3d6
bela333 <b3kstudio@gmail.com>
2021-10-08 19:14:12 +0200

    Render image

--- raytracer/archyrt-dev/src/main.rs
+++ raytracer/archyrt-dev/src/main.rs
@@ -1,6 +1,13 @@
-use archyrt_core::vector;
+use archyrt_core::{
+    api::fragment_collector::FragmentCollector, collector::image_collector::ImageCollector,
+    renderers::basic_renderer::BasicRenderer,
+};
 
 fn main() {
-    let v = vector!(1.0, 2.0, 3.0);
-    println!("{:?}", v);
+    let renderer = BasicRenderer {
+        ..Default::default()
+    };
+    let collector = ImageCollector {};
+    let image = collector.collect(renderer, 1024, 1024).unwrap();
+    image.save("image.png").unwrap();
 }
