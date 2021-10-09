7048f630ae7d63c438b856448560d6462919cbc1
GArnold02 <garnold02@gmail.com>
2021-10-09 10:30:33 +0200

    Added test mesh

--- viewport/logic/src/lib.rs
+++ viewport/logic/src/lib.rs
@@ -1,7 +1,11 @@
 mod input;
 
 use input::InputMapper;
-use tools::app::{App, MainLoop};
+use tools::{
+    app::{App, MainLoop},
+    gfx::{Mesh, Tri, Vert},
+    math::{Mat4, Vec3},
+};
 
 pub struct Viewport {
     input_mapper: InputMapper,
@@ -15,5 +19,33 @@ impl Default for Viewport {
 }
 
 impl MainLoop for Viewport {
-    fn process(&mut self, app: &mut App) {}
+    fn process(&mut self, app: &mut App) {
+        let mesh = Mesh::new(
+            app.graphics(),
+            &[
+                Vert {
+                    pos: [0.0, 0.0, 0.0],
+                    normal: [0.0, 0.0, 0.0],
+                    uv: [0.0, 0.0],
+                },
+                Vert {
+                    pos: [1.0, 0.0, 0.0],
+                    normal: [0.0, 0.0, 1.0],
+                    uv: [1.0, 0.0],
+                },
+                Vert {
+                    pos: [1.0, 1.0, 0.0],
+                    normal: [0.0, 0.0, 1.0],
+                    uv: [1.0, 1.0],
+                },
+                Vert {
+                    pos: [0.0, 1.0, 0.0],
+                    normal: [0.0, 0.0, 1.0],
+                    uv: [0.0, 1.0],
+                },
+            ],
+            &[Tri { idx: [0, 1, 2] }, Tri { idx: [0, 2, 3] }],
+        );
+        mesh.draw(app.graphics(), Mat4::translation(Vec3::new(0.0, 0.0, -2.0)));
+    }
 }
