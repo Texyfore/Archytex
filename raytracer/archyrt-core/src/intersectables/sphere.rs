aa2c929909db4d310f5ca5c35e262ac9dc8bc79e
bela333 <b3kstudio@gmail.com>
2021-10-09 16:01:51 +0200

    Refactor sphere intersection

--- raytracer/archyrt-core/src/intersectables/sphere.rs
+++ raytracer/archyrt-core/src/intersectables/sphere.rs
@@ -53,24 +53,11 @@ impl Intersectable for Sphere {
     fn intersect(&self, ray: Ray) -> Option<Intersection> {
         //Solving the equation |ray.origin+ray.direction*t-self.origin|=self.radius for t
         //Rewritten: t^2*(ray.direction^2) + t*(2*ray.origin*ray.direction-2*ray.direction*self.origin)+(ray.origin^2+self.origin^2-2*ray.origin*self.origin)=self.radius^2
-        let t_: Vec<f64> = (0..3)
-            .into_iter()
-            .map(|i| ray.direction[i] * ray.direction[i])
-            .collect();
-        let t2 = t_.iter().sum();
-        let t: f64 = (0..3)
-            .into_iter()
-            .map(|i| {
-                2.0 * ray.origin[i] * ray.direction[i] - 2.0 * ray.direction[i] * self.origin[i]
-            })
-            .sum();
-        let c: f64 = (0..3)
-            .map(|i| {
-                ray.origin[i] * ray.origin[i] + self.origin[i] * self.origin[i]
-                    - 2.0 * ray.origin[i] * self.origin[i]
-            })
-            .sum::<f64>()
-            - self.radius * self.radius;
+        let t2 = 1.0; //Assuming ray is normalized. Otherwise ray.direction.length_squared()
+        let t = 2.0 * ray.direction.dot(ray.origin - self.origin);
+        let c = ray.origin.length_squared() + self.origin.length_squared()
+            - 2.0 * ray.origin.dot(self.origin)
+            - self.radius.powi(2);
         let solutions = solve_quadratic(t2, t, c);
         let distance = find_closest(solutions)?;
         let pos = ray.direction * distance + ray.origin;
