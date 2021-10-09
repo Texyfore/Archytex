c7107312177349aba7a8ad2a23236a8430085ef0
bela333 <b3kstudio@gmail.com>
2021-10-09 22:37:41 +0200

    BasicRenderer benchmark

--- raytracer/archyrt-core/benches/core_benchmark.rs
+++ raytracer/archyrt-core/benches/core_benchmark.rs
@@ -1,5 +1,8 @@
 use archyrt_core::{
+    api::fragment_collector::FragmentCollector,
+    collector::image_collector::ImageCollector,
     intersectables::sphere::Sphere,
+    renderers::basic_renderer::BasicRenderer,
     utilities::{
         math::Vec3,
         ray::{Intersectable, Ray},
@@ -24,5 +27,18 @@ pub fn sphere_ray_intersection(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, sphere_ray_intersection);
+pub fn rendering(c: &mut Criterion) {
+    c.bench_function("rendering", |b| {
+        b.iter(|| {
+            let renderer = BasicRenderer {
+                ..Default::default()
+            };
+            let collector = ImageCollector {};
+            let image = collector.collect(renderer, 10, 10).unwrap();
+            black_box(image);
+        })
+    });
+}
+
+criterion_group!(benches, sphere_ray_intersection, rendering);
 criterion_main!(benches);
