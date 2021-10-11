0436304c55c69c84cbe71b71ad5d822acbfc2ca9
bela333 <b3kstudio@gmail.com>
2021-10-11 12:15:51 +0200

    Modify Main to test Triangle

--- raytracer/archyrt-dev/src/main.rs
+++ raytracer/archyrt-dev/src/main.rs
@@ -1,18 +1,17 @@
 use archyrt_core::{
-    api::fragment_collector::FragmentCollector, cameras::perspective::PerspectiveCamera,
-    collector::image_collector::ImageCollector, intersectables::surface::Surface,
-    renderers::basic_renderer::BasicRenderer, utilities::math::Vec3,
+    api::fragment_collector::FragmentCollector,
+    cameras::perspective::PerspectiveCamera,
+    collector::image_collector::ImageCollector,
+    intersectables::triangle::Triangle,
+    renderers::basic_renderer::BasicRenderer,
+    utilities::math::Vec3,
 };
 
 fn main() {
     let renderer = BasicRenderer {
-        object: Surface {
-            distance: -4.0,
-            normal: Vec3::new(0.0, 1.0, 0.0),
-            color: Vec3::from_single(1.0),
-        },
+        object: Triangle::default(),
         camera: PerspectiveCamera::new(Vec3::default(), Vec3::new(0.0, 0.0, 1.0), 1.0),
-        lamp: Vec3::new(0.0, 1.0, 40.0),
+        lamp: Vec3::new(0.0, 0.0, 0.0),
     };
     let collector = ImageCollector {};
     let image = collector.collect(renderer, 1024, 1024).unwrap();
