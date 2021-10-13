c1cb971a83fa76eb8a83471201f0e09ecef0a116
GArnold02 <garnold02@gmail.com>
2021-10-13 09:22:30 +0200

    Basic polygon editing

--- viewport/logic/src/lib.rs
+++ viewport/logic/src/lib.rs
@@ -2,9 +2,9 @@ mod input;
 mod model;
 
 use input::InputMapper;
+use model::{Points, Polygon};
 use tools::{
     app::{event::Event, input::ButtonKind, App, MainLoop},
-    gfx::{LineMesh, LineVert},
     math::{InnerSpace, Mat4, SquareMatrix, Vec3, Vec4},
 };
 
@@ -12,7 +12,8 @@ use crate::input::ElementKind;
 
 pub struct Viewport {
     input_mapper: InputMapper,
-    points: Vec<Vec3>,
+    points: Points,
+    poly_points: Vec<u16>,
 }
 
 impl Default for Viewport {
@@ -20,6 +21,7 @@ impl Default for Viewport {
         Self {
             input_mapper: Default::default(),
             points: Default::default(),
+            poly_points: Default::default(),
         }
     }
 }
@@ -37,43 +39,30 @@ impl MainLoop for Viewport {
         }
 
         if self.input_mapper.query_action_once("add_point") {
-            let projection =
-                tools::math::perspective(tools::math::Deg(60.0), 1024.0 / 768.0, 0.1, 100.0);
-
-            let (x, y) = self.input_mapper.query_mouse_pos();
-            let (x, y) = ((x / 1024.0 - 0.5) * 2.0, ((768.0 - y) / 768.0 - 0.5) * 2.0);
-
-            let beg = unproject(Vec3::new(x, y, -1.0), projection);
-            let end = unproject(Vec3::new(x, y, 1.0), projection);
-
-            let dir = (end - beg).normalize();
-            let point = beg + dir * 5.0;
-            self.points.push(point);
+            let point = mouse_to_3d(self.input_mapper.query_mouse_pos());
+            self.poly_points.push(self.points.insert(point));
         }
 
-        if !self.points.is_empty() {
-            let points = {
-                let mut vec = self.points.clone();
-                vec.push(self.points[0]);
-                vec
-            };
-
-            LineMesh::new(
-                app.graphics(),
-                &points
-                    .windows(2)
-                    .flatten()
-                    .map(|p| LineVert {
-                        pos: [p.x, p.y, p.z],
-                    })
-                    .collect::<Vec<_>>(),
-            )
+        let polygon = Polygon::new(self.poly_points.clone(), vec![]);
+        polygon
+            .gen_wireframe(&self.points, app.graphics())
             .draw(app.graphics(), Mat4::identity());
-        }
     }
 }
 
-fn unproject(point: Vec3, mat: Mat4) -> Vec3 {
+fn mouse_to_3d(mouse: (f32, f32)) -> Vec3 {
+    let (x, y) = mouse;
+    let (x, y) = ((x / 1024.0 - 0.5) * 2.0, ((768.0 - y) / 768.0 - 0.5) * 2.0);
+
+    let beg = unproject(Vec3::new(x, y, -1.0));
+    let end = unproject(Vec3::new(x, y, 1.0));
+
+    let dir = (end - beg).normalize();
+    beg + dir * 5.0
+}
+
+fn unproject(point: Vec3) -> Vec3 {
+    let mat = tools::math::perspective(tools::math::Deg(60.0), 1024.0 / 768.0, 0.1, 100.0);
     let point = mat.invert().unwrap() * Vec4::new(point.x, point.y, point.z, 1.0);
     Vec3::new(point.x / point.w, point.y / point.w, point.z / point.w)
 }
