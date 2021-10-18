e6ecf9f1440becc89a6f009a2721e6e02bf938db
GArnold02 <garnold02@gmail.com>
2021-10-18 09:09:58 +0200

    Queriable mouse delta

--- viewport/logic/src/input.rs
+++ viewport/logic/src/input.rs
@@ -1,16 +1,32 @@
 use std::{cell::Cell, collections::HashMap};
-use tools::app::{
-    event::RawInputKind,
-    input::{ButtonKind, InputState, KeyKind},
+use tools::{
+    app::{
+        event::RawInputKind,
+        input::{ButtonKind, InputState, KeyKind},
+    },
+    math::{Vector2, Zero},
 };
 
-#[derive(Default)]
 pub struct InputMapper {
     key_state: HashMap<KeyKind, InputState>,
     button_state: HashMap<ButtonKind, InputState>,
-    mouse_pos: (f32, f32),
+    mouse_pos_before: Vector2<f32>,
+    mouse_pos: Vector2<f32>,
     wheel_delta: f32,
     actions: HashMap<String, Action>,
+}
+
+impl Default for InputMapper {
+    fn default() -> Self {
+        Self {
+            key_state: Default::default(),
+            button_state: Default::default(),
+            mouse_pos_before: Vector2::zero(),
+            mouse_pos: Vector2::zero(),
+            wheel_delta: 0.0,
+            actions: Default::default(),
+        }
+    }
 }
 
 impl InputMapper {
@@ -23,7 +39,7 @@ impl InputMapper {
                 self.button_state.insert(button, state);
             }
             RawInputKind::Movement(x, y) => {
-                self.mouse_pos = (x, y);
+                self.mouse_pos = Vector2::new(x, y);
             }
             RawInputKind::Wheel(delta) => {
                 self.wheel_delta = delta;
@@ -33,6 +49,7 @@ impl InputMapper {
 
     pub fn clear(&mut self) {
         self.wheel_delta = 0.0;
+        self.mouse_pos_before = self.mouse_pos
     }
 
     pub fn register_action(&mut self, name: &str, elements: Vec<ElementKind>) {
@@ -66,8 +83,12 @@ impl InputMapper {
         false
     }
 
-    pub fn query_mouse_pos(&self) -> (f32, f32) {
+    pub fn query_mouse_pos(&self) -> Vector2<f32> {
         self.mouse_pos
+    }
+
+    pub fn query_mouse_delta(&self) -> Vector2<f32> {
+        self.mouse_pos - self.mouse_pos_before
     }
 
     pub fn query_wheel_delta(&self) -> f32 {
