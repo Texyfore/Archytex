b841f8284561dbcb9b88bde95f6be580e84871ef
YertofeX <szabodaniel35@gmail.com>
2021-10-27 19:19:58 +0200

    DashboardAppBar buttons and tooltips

--- frontend/src/components/dasboard-components/DashboardAppBar.tsx
+++ frontend/src/components/dasboard-components/DashboardAppBar.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { Close, MenuOutlined } from "@mui/icons-material";
+import {
+  Close,
+  Logout,
+  MenuOutlined,
+  PersonAdd,
+  Settings,
+} from "@mui/icons-material";
 import {
   AppBar,
   Avatar,
@@ -7,6 +13,12 @@ import {
   Toolbar,
   Typography,
   Box,
+  Tooltip,
+  Button,
+  Menu,
+  MenuItem,
+  Divider,
+  ListItemIcon,
 } from "@mui/material";
 import ArchytexIcon from "../ArchytexIcon";
 import { styled } from "@mui/material/styles";
@@ -25,6 +37,17 @@ function DashboardAppBar({ open, handleOpenChange }: AppBarProps) {
     handleOpenChange(!open);
   };
 
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const avatarMenuOpen = Boolean(anchorEl);
+  const handleAvatarMenuClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleAvatarMenuClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <CustomAppBar position='fixed'>
       <Toolbar sx={{ justifyContent: "space-between" }}>
@@ -33,16 +56,18 @@ function DashboardAppBar({ open, handleOpenChange }: AppBarProps) {
             {open ? <Close /> : <MenuOutlined />}
           </IconButton>
         </Box>
-        <Box display={{ xs: "none", md: "flex" }}>
-          <ArchytexIcon />
-          <Typography
-            variant='h6'
-            component='h2'
-            sx={{ display: { xs: "none", sm: "block" }, paddingTop: 0.4 }}
-          >
-            ARCHYTEX
-          </Typography>
-        </Box>
+        <Tooltip title='Archytex version 0.0.1' placement='right'>
+          <Box display={{ xs: "none", md: "flex" }}>
+            <ArchytexIcon />
+            <Typography
+              variant='h6'
+              component='h2'
+              sx={{ display: { xs: "none", sm: "block" }, paddingTop: 0.5 }}
+            >
+              ARCHYTEX
+            </Typography>
+          </Box>
+        </Tooltip>
         <Typography
           variant='h6'
           component='div'
@@ -53,14 +78,59 @@ function DashboardAppBar({ open, handleOpenChange }: AppBarProps) {
           DASHBOARD
         </Typography>
         <Box sx={{ display: "flex" }}>
-          <Typography
-            fontSize={22}
-            fontWeight={400}
-            sx={{ display: { xs: "none", md: "block" }, paddingTop: 0.4 }}
+          <Button
+            onClick={handleAvatarMenuClick}
+            variant='text'
+            disableRipple
+            disableFocusRipple
+            sx={{
+              marginY: 1,
+              textTransform: "none",
+              display: { xs: "none", md: "block" },
+            }}
+            color='inherit'
           >
-            Test User
-          </Typography>
-          <Avatar sx={{ backgroundColor: "#39A0ED", marginLeft: 2 }} />
+            <Typography fontSize={20} fontWeight={400}>
+              Test User
+            </Typography>
+          </Button>
+          <IconButton onClick={handleAvatarMenuClick}>
+            <Avatar sx={{ backgroundColor: "#39A0ED" }} />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={avatarMenuOpen}
+            onClose={handleAvatarMenuClose}
+            PaperProps={{
+              elevation: 0,
+              sx: {
+                overflow: "visible",
+                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                mt: 1.5,
+                "&:before": {
+                  content: '""',
+                  display: "block",
+                  position: "absolute",
+                  top: 0,
+                  right: 25,
+                  width: 10,
+                  height: 10,
+                  bgcolor: "background.paper",
+                  transform: "translateY(-50%) rotate(45deg)",
+                  zIndex: 0,
+                },
+              },
+            }}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+          >
+            <MenuItem>
+              <ListItemIcon>
+                <Logout fontSize='small' />
+              </ListItemIcon>
+              Logout
+            </MenuItem>
+          </Menu>
         </Box>
       </Toolbar>
     </CustomAppBar>
