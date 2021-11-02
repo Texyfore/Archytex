f2de0a504c658f0edb2669ca9fc70e6ed98c50b0
GArnold02 <gulyas.arnold02@protonmail.com>
2021-11-02 11:25:00 +0100

    Better debugger

--- viewport/build.sh
+++ viewport/build.sh
@@ -1,2 +1,3 @@
-wasm-pack build entry --target=web --no-typescript
-echo -e "*.wasm\n*.js\npackage.json\n.netlify" > entry/pkg/.gitignore
+wasm-pack build entry --target=web --no-typescript --release
+cp -r entry/pkg out
+cp index.html out/index.html
