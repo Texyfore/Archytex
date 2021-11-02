5b7b80ba10da41f98aa9c2167a4a7cb4a0a53149
GArnold02 <gulyas.arnold02@protonmail.com>
2021-11-02 11:26:33 +0100

    Basic sendMessage() implementation

--- viewport/entry/src/lib.rs
+++ viewport/entry/src/lib.rs
@@ -1,19 +1,15 @@
 use logic::Viewport;
-use tools::app::App;
+use tools::{app::App, console};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn main() {
-    run();
-}
-
-#[wasm_bindgen]
-pub fn send_message(msg: &str) {
-
-}
-
-fn run() {
     let app = App::default();
     let viewport = Viewport::default();
     app.run(viewport);
+}
+
+#[wasm_bindgen(js_name = "sendMessage")]
+pub fn message_received(msg: &str) {
+    console!("received message: `{}`", msg)
 }
