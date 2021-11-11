04b08b4da61d7a3276be6422b56468ee6cebac78
GArnold02 <gulyas.arnold02@protonmail.com>
2021-11-11 08:52:42 +0100

    Windowing

--- viewport/src/main.rs
+++ viewport/src/main.rs
@@ -1,6 +1,44 @@
+use winit::{
+    dpi::PhysicalSize,
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::{Window, WindowBuilder},
+};
+
 mod log;
 
 fn main() {
+    #[cfg(target_arch = "wasm32")]
     console_error_panic_hook::set_once();
-    info!("Hello, World!");
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::default().build(&event_loop).unwrap();
+
+    #[cfg(target_arch = "wasm32")]
+    insert_canvas(&window);
+
+    event_loop.run(|event, _, flow| {
+        *flow = ControlFlow::Poll;
+        match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::Resized(PhysicalSize { width, height }) => {}
+                _ => {}
+            },
+            Event::MainEventsCleared => {}
+            _ => {}
+        }
+    });
+}
+
+#[cfg(target_arch = "wasm32")]
+fn insert_canvas(window: &Window) {
+    use winit::platform::web::WindowExtWebSys;
+    web_sys::window()
+        .unwrap()
+        .document()
+        .unwrap()
+        .body()
+        .unwrap()
+        .append_child(&window.canvas())
+        .unwrap();
 }
