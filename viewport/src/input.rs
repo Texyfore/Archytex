945cc94724792f77aa3cf1491f134c21a2ea88fb
GArnold02 <gulyas.arnold02@protonmail.com>
2021-11-12 13:12:30 +0100

    Added input mapper

---
+++ viewport/src/input.rs
@@ -0,0 +1,96 @@
+use std::collections::HashMap;
+
+use winit::event::{ElementState, MouseButton, VirtualKeyCode};
+
+pub trait Input {
+    fn is_active(&self, action: &str) -> bool;
+    fn is_active_once(&self, action: &str) -> bool;
+}
+
+pub struct InputMapper {
+    actions: HashMap<String, Action>,
+}
+
+impl InputMapper {
+    pub fn new(actions: &[(&str, Trigger)]) -> Self {
+        Self {
+            actions: actions
+                .iter()
+                .map(|(name, trigger)| {
+                    (
+                        name.to_string(),
+                        Action {
+                            trigger: trigger.clone(),
+                            active: false,
+                            active_once: false,
+                        },
+                    )
+                })
+                .collect(),
+        }
+    }
+
+    pub fn register_action(&mut self, name: String, trigger: Trigger) {
+        self.actions.insert(
+            name,
+            Action {
+                trigger,
+                active: false,
+                active_once: false,
+            },
+        );
+    }
+
+    pub fn set_trigger(&mut self, trigger: Trigger, state: ElementState) {
+        for (_, mut action) in &mut self.actions {
+            if action.trigger == trigger {
+                match state {
+                    ElementState::Pressed => {
+                        action.active = true;
+                        action.active_once = true;
+                    }
+                    ElementState::Released => {
+                        action.active = false;
+                        action.active_once = false;
+                    }
+                }
+            }
+        }
+    }
+
+    pub fn tick(&mut self) {
+        for (_, mut action) in &mut self.actions {
+            action.active_once = false;
+        }
+    }
+}
+
+impl Input for InputMapper {
+    fn is_active(&self, action: &str) -> bool {
+        if let Some(action) = self.actions.get(action) {
+            action.active
+        } else {
+            false
+        }
+    }
+
+    fn is_active_once(&self, action: &str) -> bool {
+        if let Some(action) = self.actions.get(action) {
+            action.active_once
+        } else {
+            false
+        }
+    }
+}
+
+#[derive(PartialEq, Clone)]
+pub enum Trigger {
+    Key(VirtualKeyCode),
+    Button(MouseButton),
+}
+
+struct Action {
+    trigger: Trigger,
+    active: bool,
+    active_once: bool,
+}
--- viewport/src/main.rs
+++ viewport/src/main.rs
@@ -1,14 +1,16 @@
+mod input;
 mod log;
 mod render;
 
-use cgmath::{vec3, Matrix4};
-use render::{data::LineVertex, Renderer};
+use render::Renderer;
 use winit::{
     dpi::PhysicalSize,
-    event::{Event, WindowEvent},
+    event::{ElementState, Event, KeyboardInput, MouseButton, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::{Window, WindowBuilder},
 };
+
+use self::input::{InputMapper, Trigger};
 
 fn main() {
     #[cfg(target_arch = "wasm32")]
@@ -20,40 +22,36 @@ fn main() {
     #[cfg(target_arch = "wasm32")]
     insert_canvas(&window);
 
-    let mut renderer = Renderer::new(&window);
-
-    {
-        let (width, height) = window.inner_size().into();
-        renderer.resize(width, height);
-    }
-
-    renderer.update_wireframe(&[
-        LineVertex {
-            position: [0.0, 0.0, 0.0],
-            color: [1.0, 0.0, 0.0, 1.0],
-        },
-        LineVertex {
-            position: [1.0, 1.0, 0.0],
-            color: [1.0, 0.0, 0.0, 1.0],
-        },
-    ]);
-
-    renderer.update_camera_view(Matrix4::from_translation(vec3(0.0, 0.0, 5.0)));
+    let mut main_loop = MainLoop::init(window);
 
     event_loop.run(move |event, _, flow| {
         *flow = ControlFlow::Poll;
         match event {
             Event::WindowEvent { event, .. } => match event {
                 WindowEvent::Resized(PhysicalSize { width, height }) => {
-                    renderer.resize(width, height);
+                    main_loop.window_resized(width, height);
                 }
                 WindowEvent::CloseRequested => {
                     *flow = ControlFlow::Exit;
                 }
+                WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            virtual_keycode: Some(code),
+                            state,
+                            ..
+                        },
+                    ..
+                } => {
+                    main_loop.keyboard_input(code, state);
+                }
+                WindowEvent::MouseInput { button, state, .. } => {
+                    main_loop.mouse_input(button, state);
+                }
                 _ => {}
             },
             Event::MainEventsCleared => {
-                renderer.render();
+                main_loop.process();
             }
             _ => {}
         }
@@ -71,4 +69,46 @@ fn insert_canvas(window: &Window) {
         .unwrap()
         .append_child(&window.canvas())
         .unwrap();
+}
+
+struct MainLoop {
+    window: Window,
+    renderer: Renderer,
+    input_mapper: InputMapper,
+}
+
+impl MainLoop {
+    fn init(window: Window) -> Self {
+        let mut renderer = Renderer::new(&window);
+        let input_mapper = InputMapper::new(&[]);
+
+        {
+            let (width, height) = window.inner_size().into();
+            renderer.resize(width, height);
+        }
+
+        Self {
+            window,
+            renderer,
+            input_mapper,
+        }
+    }
+
+    fn window_resized(&mut self, width: u32, height: u32) {
+        self.renderer.resize(width, height);
+    }
+
+    fn keyboard_input(&mut self, code: VirtualKeyCode, state: ElementState) {
+        self.input_mapper.set_trigger(Trigger::Key(code), state);
+    }
+
+    fn mouse_input(&mut self, button: MouseButton, state: ElementState) {
+        self.input_mapper
+            .set_trigger(Trigger::Button(button), state);
+    }
+
+    fn process(&mut self) {
+        self.input_mapper.tick();
+        self.renderer.render();
+    }
 }
