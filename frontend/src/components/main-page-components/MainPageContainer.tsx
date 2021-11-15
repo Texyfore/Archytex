92e23d5d3bc439ababeef22d2339d66cd942fc6c
YertofeX <szabodaniel35@gmail.com>
2021-11-15 18:28:35 +0100

    Main Page Content Card and Container components

---
+++ frontend/src/components/main-page-components/MainPageContainer.tsx
@@ -0,0 +1,21 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+export default function MainPageContainer({
+  children,
+}: {
+  children: JSX.Element;
+}) {
+  return (
+    <Box
+      display='flex'
+      justifyContent='space-evenly'
+      alignItems='center'
+      flexDirection={{ xs: "column", md: "row" }}
+      marginX={{ xs: 2, md: 10, lg: 20, xl: 40 }}
+      marginY='10vh'
+    >
+      {children}
+    </Box>
+  );
+}
