350081f5dd7a1ea346f6cc3f605c5fc9c4dd1162
GArnold02 <gulyas.arnold02@protonmail.com>
2021-11-15 10:01:29 +0100

    Per-face texturing

--- viewport/src/editor/brush.rs
+++ viewport/src/editor/brush.rs
@@ -1,20 +1,34 @@
-use std::rc::Rc;
+use std::{collections::HashMap, rc::Rc};
 
 use cgmath::{vec3, InnerSpace, Matrix4, Quaternion, Vector3};
 
 use crate::render::{
-    data::BrushVertex, BrushCommand, BrushComponent, BrushMesh, GraphicsWorld, Texture, Transform,
+    data::{BrushVertex, Triangle},
+    BrushCommand, BrushComponent, BrushMesh, GraphicsWorld, Texture, Transform,
 };
 
+pub type Point = Vector3<f32>;
+
 pub struct Brush {
-    points: Vec<Vector3<f32>>,
-    faces: Vec<[u16; 4]>,
+    points: Vec<Point>,
+    textures: Vec<Rc<Texture>>,
+    faces: Vec<Face>,
     transform: Rc<Transform>,
-    mesh_cache: Vec<Rc<BrushMesh>>,
+    mesh_cache: HashMap<usize, Rc<BrushMesh>>,
+}
+
+struct Face {
+    idx: [u16; 4],
+    texture: usize,
 }
 
 impl Brush {
-    pub fn new<G: GraphicsWorld>(gfx: &G, extent: Vector3<f32>, transform: Matrix4<f32>) -> Self {
+    pub fn new<G: GraphicsWorld>(
+        gfx: &G,
+        extent: Vector3<f32>,
+        transform: Matrix4<f32>,
+        texture: Rc<Texture>,
+    ) -> Self {
         let points = vec![
             vec3(0.0, 0.0, 0.0),
             vec3(extent.x, 0.0, 0.0),
@@ -33,10 +47,19 @@ impl Brush {
             [6, 7, 3, 2],
             [0, 3, 7, 4],
             [5, 6, 2, 1],
-        ];
+        ]
+        .iter()
+        .map(|idx| Face {
+            idx: *idx,
+            texture: 0,
+        })
+        .collect();
+
+        let textures = vec![texture];
 
         Self {
             points,
+            textures,
             faces,
             transform: gfx.create_transform(transform),
             mesh_cache: Default::default(),
@@ -47,20 +70,46 @@ impl Brush {
         self.points[idx as usize] = point;
     }
 
+    pub fn set_texture(&mut self, face: u16, texture: Rc<Texture>) {
+        // TODO: This is really, really bad. But at the moment, the compiler bug
+        // forces me to do it this way.
+
+        for i in 0..self.textures.len() {
+            let ptr_a = self.textures[i].as_ref() as *const Texture;
+            let ptr_b = texture.as_ref() as *const Texture;
+            if ptr_a == ptr_b {
+                self.faces[face as usize].texture = i;
+                return;
+            }
+        }
+
+        self.faces[face as usize].texture = self.textures.len();
+        self.textures.push(texture);
+    }
+
     pub fn regenerate<G: GraphicsWorld>(&mut self, gfx: &G) {
-        let mut vertices = Vec::with_capacity(self.faces.len() * 4);
-        let mut triangles = Vec::with_capacity(self.faces.len() * 2);
+        let mut geometry: HashMap<usize, (Vec<BrushVertex>, Vec<Triangle>)> = HashMap::new();
 
         for face in &self.faces {
+            let (vertices, triangles) = {
+                if !geometry.contains_key(&face.texture) {
+                    geometry.insert(face.texture, (Vec::new(), Vec::new()));
+                }
+
+                geometry.get_mut(&face.texture).unwrap()
+            };
+
             let t0 = vertices.len() as u16;
             triangles.push([t0, t0 + 1, t0 + 2]);
             triangles.push([t0, t0 + 2, t0 + 3]);
 
+            let idx = &face.idx;
+
             let p = [
-                self.points[face[0] as usize],
-                self.points[face[1] as usize],
-                self.points[face[2] as usize],
-                self.points[face[3] as usize],
+                self.points[idx[0] as usize],
+                self.points[idx[1] as usize],
+                self.points[idx[2] as usize],
+                self.points[idx[3] as usize],
             ];
 
             let edge0 = p[1] - p[0];
@@ -79,22 +128,23 @@ impl Brush {
             }
         }
 
-        self.mesh_cache.clear();
-        self.mesh_cache
-            .push(gfx.create_brush_mesh(&vertices, &triangles));
+        self.mesh_cache = geometry
+            .iter()
+            .map(|(k, (v, t))| (*k, gfx.create_brush_mesh(&v, &t)))
+            .collect();
     }
 
-    pub fn draw<G: GraphicsWorld>(&self, gfx: &mut G, texture: Rc<Texture>) {
+    pub fn draw<G: GraphicsWorld>(&self, gfx: &mut G) {
         gfx.draw_brush(BrushCommand {
             transform: self.transform.clone(),
             components: self
                 .mesh_cache
                 .iter()
-                .map(|mesh| BrushComponent {
+                .map(|(texture, mesh)| BrushComponent {
                     mesh: mesh.clone(),
-                    texture: texture.clone(),
+                    texture: self.textures[*texture].clone(),
                 })
                 .collect(),
-        })
+        });
     }
 }
