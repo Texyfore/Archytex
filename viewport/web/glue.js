d4ee15cac5bc09b2b108bcb68abf944e69da5870
garnold02 <gulyas.arnold02@protonmail.com>
2021-11-27 16:49:14 +0100

    Removed testing stuff

--- viewport/web/glue.js
+++ viewport/web/glue.js
@@ -1,4 +1,4 @@
-import init, {addTexture} from "./app/viewport.js";
+import init from "./app/viewport.js";
 
 addEventListener("load", () => {
     init();
@@ -7,16 +7,4 @@ addEventListener("contextmenu", e => e.preventDefault());
 
 export function handleMessage(msg) {
     alert(msg);
-}
-
-export function downloadAndAddTexture(id, url) {
-    fetch(url).then(r => {
-        r.blob().then(b => {
-            let reader = new FileReader();
-            reader.readAsArrayBuffer(b);
-            reader.onload = () => {
-                addTexture(id, new Uint8Array(reader.result));
-            }
-        })
-    });
 }
