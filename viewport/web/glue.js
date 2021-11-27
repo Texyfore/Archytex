bf1b562c6da2ed4ef32738aa2e272df413852361
garnold02 <gulyas.arnold02@protonmail.com>
2021-11-27 13:17:35 +0100

    addTexture works

--- viewport/web/glue.js
+++ viewport/web/glue.js
@@ -1,4 +1,4 @@
-import init from "./app/viewport.js";
+import init, {addTexture} from "./app/viewport.js";
 
 addEventListener("load", () => {
     init();
@@ -7,4 +7,16 @@ addEventListener("contextmenu", e => e.preventDefault());
 
 export function handleMessage(msg) {
     alert(msg);
+}
+
+export function downloadAndAddTexture(id, url) {
+    fetch(url).then(r => {
+        r.blob().then(b => {
+            let reader = new FileReader();
+            reader.readAsArrayBuffer(b);
+            reader.onload = () => {
+                addTexture(id, new Uint8Array(reader.result));
+            }
+        })
+    });
 }
