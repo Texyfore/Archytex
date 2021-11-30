83c5ee1191560aca04365d20ec1e7307de558632
bela333 <b3kstudio@gmail.com>
2021-11-30 16:57:30 +0100

    Add Captcha check

--- backend/routes/register.go
+++ backend/routes/register.go
@@ -12,6 +12,12 @@ type registerRequest struct {
 	Username *string `required:"true" json:"username"`
 	Email    *string `required:"true" json:"email"`
 	Password *string `required:"true" json:"password"`
+	Captcha  *string `required:"true" json:"captcha"`
+}
+
+func CheckCaptcha(token string) (bool, error) {
+	//TODO: Check captcha
+	return token == "GOOD_TOKEN", nil
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
@@ -26,5 +32,13 @@ func Register(w http.ResponseWriter, r *http.Request) {
 		logging.Error(w, r, err, err.Error(), http.StatusBadRequest)
 		return
 	}
-
+	check, err := CheckCaptcha(*data.Captcha)
+	if err != nil {
+		logging.Error(w, r, err, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	if !check {
+		logging.Error(w, r, nil, "Invalid captcha", http.StatusForbidden)
+		return
+	}
 }
