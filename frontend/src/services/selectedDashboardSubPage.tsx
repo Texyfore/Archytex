8120ce8627f275bc072fd58cf5d8281c0467c07c
YertofeX <szabodaniel35@gmail.com>
2021-12-06 12:22:23 +0100

    Dashboard navigation buttons work

---
+++ frontend/src/services/selectedDashboardSubPage.tsx
@@ -0,0 +1,23 @@
+import React, { useContext } from "react";
+
+type SubPage = "projects" | "settings" | "community";
+
+const SubPageContext = React.createContext<
+  [SubPage, React.Dispatch<React.SetStateAction<SubPage>>]
+>(["projects", () => {}]);
+
+function useSubPage() {
+  return useContext(SubPageContext);
+}
+
+function SubPageProvider({ children }: { children: JSX.Element }): JSX.Element {
+  const value: [SubPage, React.Dispatch<React.SetStateAction<SubPage>>] =
+    React.useState("projects" as SubPage);
+
+  return (
+    <SubPageContext.Provider value={value}>{children}</SubPageContext.Provider>
+  );
+}
+
+export { SubPageProvider, SubPageContext, useSubPage };
+export type { SubPage };
