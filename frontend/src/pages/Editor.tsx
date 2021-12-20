46ff51443f0309d2f457cf12831bb7c3505866ec
garnold02 <gulyas.arnold02@protonmail.com>
2021-12-20 09:38:34 +0100

    Size change works

--- frontend/src/pages/Editor.tsx
+++ frontend/src/pages/Editor.tsx
@@ -3,22 +3,32 @@ import React, { useEffect } from "react";
 import EditorMenu from "../components/editor-components/EditorMenu";
 import EditorAppBar from "../components/editor-components/EditorAppBar";
 import AppBarOffset from "../components/AppBarOffset";
-import Viewport from "../components/editor-components/Viewport";
 import EditorHandle from "../EditorUtils";
+import useDimensions from "react-cool-dimensions";
 
 const appBarHeight = 48;
+let editorHandle: EditorHandle;
 
 export default function Editor() {
-  useEffect(() => {}, []);
+  const { observe } = useDimensions({
+    onResize: ({ width, height }) => {
+      editorHandle.setResolution(width, height);
+    },
+  });
+
+  useEffect(() => {
+    editorHandle = new EditorHandle();
+  }, []);
 
   return (
     <React.Fragment>
       <EditorAppBar />
       <AppBarOffset variant="dense" />
-      <Box display="flex" height={`calc(100vh - ${appBarHeight}px)`}>
-        <Viewport />
+      <Box display="flex" height={`calc(100vh - ${appBarHeight}px)`} overflow="hidden">
+        <Box width='100%' height="100%" ref={observe} sx={{backgroundColor: '#0c0c0c'}}></Box>
         <EditorMenu />
       </Box>
+      <canvas id="viewport-canvas" style={{position: 'absolute', top: `${appBarHeight}px`}}></canvas>
     </React.Fragment>
   );
 }
