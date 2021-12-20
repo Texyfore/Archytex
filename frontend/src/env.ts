8b28645a234ff7c6d490a92d242da2c7e957d33d
garnold02 <gulyas.arnold02@protonmail.com>
2021-12-20 10:45:08 +0100

    Dynamic texture loading

--- frontend/src/env.ts
+++ frontend/src/env.ts
@@ -1,13 +1,16 @@
 interface Environment{
-    base_url: string
+    base_url: string,
+    asset_url: string
 }
 
 const dev: Environment = {
-    base_url: "http://localhost:8080/api/"
+    base_url: "http://localhost:8080/api/",
+    asset_url: "/assets"
 }
 
 const prod: Environment = {
-    base_url: "TODO"
+    base_url: "TODO",
+    asset_url: "/assets"
 };
 
 const Environment = process.env.STAGE == 'production' ? prod : dev;
--- frontend/src/pages/Editor.tsx
+++ frontend/src/pages/Editor.tsx
@@ -1,10 +1,11 @@
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import React, { useEffect } from "react";
 import EditorMenu from "../components/editor-components/EditorMenu";
 import EditorAppBar from "../components/editor-components/EditorAppBar";
 import AppBarOffset from "../components/AppBarOffset";
 import EditorHandle from "../EditorUtils";
 import useDimensions from "react-cool-dimensions";
+import Environment from "../env";
 
 const appBarHeight = 48;
 let editorHandle: EditorHandle;
@@ -20,15 +21,38 @@ export default function Editor() {
     editorHandle = new EditorHandle();
   }, []);
 
+  const handleClick = () => {
+    editorHandle.loadTexture(0, `${Environment.asset_url}/vertex.png`);
+    editorHandle.loadTexture(10, `${Environment.asset_url}/nodraw.png`);
+  };
+
   return (
     <React.Fragment>
       <EditorAppBar />
       <AppBarOffset variant="dense" />
-      <Box display="flex" height={`calc(100vh - ${appBarHeight}px)`} overflow="hidden">
-        <Box width='100%' height="100%" ref={observe} sx={{backgroundColor: '#0c0c0c'}}></Box>
+      <Box
+        display="flex"
+        height={`calc(100vh - ${appBarHeight}px)`}
+        overflow="hidden"
+      >
+        <Box
+          width="100%"
+          height="100%"
+          ref={observe}
+          sx={{ backgroundColor: "#0c0c0c" }}
+        ></Box>
         <EditorMenu />
       </Box>
-      <canvas id="viewport-canvas" style={{position: 'absolute', top: `${appBarHeight}px`}}></canvas>
+      <canvas
+        id="viewport-canvas"
+        style={{ position: "absolute", top: `${appBarHeight}px` }}
+        onContextMenu={(e) => {
+          e.preventDefault();
+        }}
+      ></canvas>
+      <Box position="absolute" top={`${appBarHeight}px`} left={10}>
+        <Button onClick={handleClick}>CLICKME</Button>
+      </Box>
     </React.Fragment>
   );
 }
