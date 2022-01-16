24c800d19dec07ab0a3d4e12aaef2eb928cf33f4
garnold02 <gulyas.arnold02@protonmail.com>
2022-01-16 15:33:21 +0100

    Improved gltf2mesh

--- tk3d/src/amdl.rs
+++ tk3d/src/amdl.rs
@@ -1,10 +1,18 @@
 use serde::{Deserialize, Serialize};
+use cgmath::Vector3;
 
 use crate::{error::DecodeError, Mesh};
 
 #[derive(Serialize, Deserialize)]
 pub struct PropModel {
+    pub bounding_box: BoundingBox,
     pub mesh: Mesh,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct BoundingBox {
+    pub min: Vector3<f32>,
+    pub max: Vector3<f32>,
 }
 
 impl PropModel {
