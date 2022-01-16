6a18aa995947927343d9321320a52320c16fea82
garnold02 <gulyas.arnold02@protonmail.com>
2022-01-16 20:27:02 +0100

    Darkened clear color

--- editor/tools/renderer/src/lib.rs
+++ editor/tools/renderer/src/lib.rs
@@ -76,7 +76,7 @@ impl Renderer {
             self.gpu
                 .set_uniform(&self.camera_uniform, &scene.camera_matrix);
 
-            let mut pass = frame.begin_pass(&self.depth_buffer, [0.1; 3]);
+            let mut pass = frame.begin_pass(&self.depth_buffer, [0.01; 3]);
             pass.set_uniform(0, &self.camera_uniform);
 
             pass.set_mesh_pipeline(&self.mesh_pipeline);
