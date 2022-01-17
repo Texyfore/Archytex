283c6f72277e6dd1514087864a8471eb9f4640c3
garnold02 <gulyas.arnold02@protonmail.com>
2022-01-17 07:59:13 +0100

    Mesh implementation

---
+++ toolkit/mesh/src/lib.rs
@@ -0,0 +1,23 @@
+use cgmath::{Vector2, Vector3};
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct Mesh {
+    pub vertices: Vec<Vertex>,
+    pub triangles: Vec<[u16; 3]>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct Vertex {
+    pub position: Vector3<f32>,
+    pub normal: Vector3<f32>,
+    pub texcoord: Vector2<f32>,
+}
+
+#[cfg(feature = "bytemuck")]
+mod bytemuck_impl {
+    use bytemuck::{Pod, Zeroable};
+    unsafe impl Zeroable for Vertex {}
+    unsafe impl Pod for Vertex {}
+}
