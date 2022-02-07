0a33d980746002115e7546d9d478009a4c698c26
garnold02alt <garnold02alt@protonmail.com>
2022-02-07 12:28:27 +0100

    Initialized workspace

---
+++ editor/packages/gpu/src/lib.rs
@@ -0,0 +1,17 @@
+mod base;
+mod buffer;
+mod depth_buffer;
+mod pass;
+mod pipeline;
+mod res;
+mod texture;
+mod uniform;
+
+pub use base::*;
+pub use buffer::*;
+pub use depth_buffer::*;
+pub use pass::*;
+pub use pipeline::*;
+pub use res::*;
+pub use texture::*;
+pub use uniform::*;
---
+++ editor/packages/gpu/src/texture.rs
@@ -0,0 +1,89 @@
+use std::num::NonZeroU32;
+
+use wgpu::{
+    AddressMode, BindGroup, BindGroupDescriptor, BindGroupEntry, BindingResource, Extent3d,
+    FilterMode, ImageCopyTexture, ImageDataLayout, Origin3d, SamplerDescriptor, TextureAspect,
+    TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
+};
+
+use crate::{res::Res, Gpu};
+
+pub use wgpu::Sampler;
+
+pub struct Texture {
+    pub(crate) group: BindGroup,
+}
+
+pub struct Image<'a> {
+    pub width: u32,
+    pub height: u32,
+    pub buf: &'a [u8],
+}
+
+impl Gpu {
+    pub fn create_texture(&self, sampler: &Sampler, image: Image) -> Res<Texture> {
+        let size = Extent3d {
+            width: image.width,
+            height: image.height,
+            depth_or_array_layers: 1,
+        };
+
+        let texture = self.device.create_texture(&TextureDescriptor {
+            label: None,
+            size,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::Rgba8UnormSrgb,
+            usage: TextureUsages::TEXTURE_BINDING | TextureUsages::COPY_DST,
+        });
+
+        let view = texture.create_view(&Default::default());
+
+        let group = self.device.create_bind_group(&BindGroupDescriptor {
+            label: None,
+            layout: &self.texture_layout,
+            entries: &[
+                BindGroupEntry {
+                    binding: 0,
+                    resource: BindingResource::TextureView(&view),
+                },
+                BindGroupEntry {
+                    binding: 1,
+                    resource: BindingResource::Sampler(sampler),
+                },
+            ],
+        });
+
+        self.queue.write_texture(
+            ImageCopyTexture {
+                texture: &texture,
+                mip_level: 0,
+                origin: Origin3d::ZERO,
+                aspect: TextureAspect::All,
+            },
+            image.buf,
+            ImageDataLayout {
+                offset: 0,
+                bytes_per_row: NonZeroU32::new(size.width * 4),
+                rows_per_image: NonZeroU32::new(size.height),
+            },
+            size,
+        );
+
+        Res::new(Texture { group })
+    }
+
+    pub fn create_sampler(&self) -> Res<Sampler> {
+        Res::new(self.device.create_sampler(&SamplerDescriptor {
+            label: None,
+            address_mode_u: AddressMode::Repeat,
+            address_mode_v: AddressMode::Repeat,
+            address_mode_w: AddressMode::Repeat,
+            mag_filter: FilterMode::Nearest,
+            min_filter: FilterMode::Nearest,
+            mipmap_filter: FilterMode::Nearest,
+            ..Default::default()
+        }))
+    }
+}
