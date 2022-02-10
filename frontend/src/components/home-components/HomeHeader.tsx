14dc7449c7ba1ae6f08c5aac27cfbe2d1cca16d5
YertofeX <szabodaniel35@gmail.com>
2022-02-10 21:18:03 +0100

    Bubble particles

--- frontend/src/components/home-components/HomeHeader.tsx
+++ frontend/src/components/home-components/HomeHeader.tsx
@@ -1,35 +1,74 @@
 import React from "react";
+
+import { useTranslation } from "react-i18next";
+
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+
 import Logo from "../general-components/Logo";
-import headerBackground from "../../img/headerBackground.svg";
+import ParticleBubbles from "./ParticleBubbles";
+
+import headerImage1 from "../../img/create_together.svg";
+import headerImage2 from "../../img/building_1.svg";
 
 export default function HomeHeader() {
+  const { t } = useTranslation();
+
+  const theme = useTheme();
+  const upMd = useMediaQuery(theme.breakpoints.up("md"));
+  const matchesXs = useMediaQuery(theme.breakpoints.only("xs"));
+
   return (
     <>
+      <ParticleBubbles />
       <Box
-        display='flex'
-        flexDirection='column'
-        justifyContent='center'
-        alignItems='center'
-        width='100%'
-        height='75vh'
+        position='absolute'
+        top={upMd ? "150px" : "300px"}
+        right={matchesXs ? "12vw" : "5vw"}
+        zIndex={0}
       >
-        <img src={headerBackground} alt='header-background' width='100%' />
+        {upMd ? (
+          <img src={headerImage1} alt='header-background' width='500px' />
+        ) : (
+          <img src={headerImage1} alt='header-background' width='300px' />
+        )}
       </Box>
+      <Box
+        position='absolute'
+        top={upMd ? "121px" : "273px"}
+        left='0'
+        zIndex={0}
+        display={{ xs: "none", sm: "inherit" }}
+      >
+        {upMd ? (
+          <img src={headerImage2} alt='header-background' width='500px' />
+        ) : (
+          <img src={headerImage2} alt='header-background' width='300px' />
+        )}
+      </Box>
+      <Box
+        position='absolute'
+        top='500px'
+        width='100%'
+        zIndex={1}
+        bgcolor='#444053'
+        height='1.5px'
+      />
       <Box
         position='absolute'
         top={0}
         width='100%'
-        height={{ xs: "50vh", md: "75vh" }}
+        height={matchesXs ? "400px" : "500px"}
         display='flex'
         flexDirection='column'
         justifyContent='center'
         alignItems='center'
       >
-        <Logo size={100} />
+        <Logo size={100} marginRight={0} />
         <Typography variant='h4' component='h1' color='initial'>
-          ARCHYTEX
+          {t("archytex").toUpperCase()}
         </Typography>
       </Box>
     </>
