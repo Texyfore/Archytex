5a93081af8879d9a6504f15a2b7d445c4decc072
garnold02alt <garnold02alt@protonmail.com>
2022-02-11 13:13:46 +0100

    Started procedural mesh

--- editor/packages/app/src/logic/element.rs
+++ editor/packages/app/src/logic/element.rs
@@ -1,7 +1,7 @@
 use asset::{PropID, TextureID};
-use cgmath::Vector3;
+use cgmath::{vec3, Vector3};
 
-use crate::graphics::{Canvas, PropData, PropInstance, Share, SolidMesh};
+use crate::graphics::{Canvas, Graphics, PropData, PropInstance, Share, SolidMesh};
 
 #[derive(Clone, Copy)]
 pub enum ElementKind {
@@ -12,15 +12,54 @@ pub enum ElementKind {
 }
 
 pub struct Solid {
-    faces: [Face; 6],
     points: [Point; 8],
+    faces: [Face; 6],
     selected: bool,
     mesh: SolidMesh,
 }
 
 impl Solid {
+    pub fn new(graphics: &Graphics, origin: Vector3<i32>, extent: Vector3<i32>) -> Self {
+        Self {
+            points: [
+                vec3(0, 0, 0).into(),
+                vec3(1, 0, 0).into(),
+                vec3(1, 0, 1).into(),
+                vec3(0, 0, 1).into(),
+                vec3(0, 1, 0).into(),
+                vec3(1, 1, 0).into(),
+                vec3(1, 1, 1).into(),
+                vec3(0, 1, 1).into(),
+            ],
+            faces: [
+                (TextureID(0), [1, 2, 6, 5]),
+                (TextureID(0), [0, 0, 0, 0]),
+                (TextureID(0), [0, 0, 0, 0]),
+                (TextureID(0), [0, 0, 0, 0]),
+                (TextureID(0), [0, 0, 0, 0]),
+                (TextureID(0), [0, 0, 0, 0]),
+            ],
+            selected: todo!(),
+            mesh: todo!(),
+        }
+    }
+
     pub fn render(&self, canvas: &mut Canvas) {
         canvas.draw_solid(self.mesh.share());
+    }
+}
+
+pub struct Point {
+    position: Vector3<i32>,
+    selected: bool,
+}
+
+impl From<Vector3<i32>> for Point {
+    fn from(position: Vector3<i32>) -> Self {
+        Self {
+            position,
+            selected: false,
+        }
     }
 }
 
@@ -30,9 +69,14 @@ pub struct Face {
     selected: bool,
 }
 
-pub struct Point {
-    position: Vector3<i32>,
-    selected: bool,
+impl From<(TextureID, [usize; 4])> for Face {
+    fn from(tuple: (TextureID, [usize; 4])) -> Self {
+        Self {
+            texture: tuple.0,
+            indices: tuple.1,
+            selected: false,
+        }
+    }
 }
 
 pub struct Prop {
