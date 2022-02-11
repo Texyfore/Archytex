19481c0e5845ecd503fc8fd33adfda873ac7c349
garnold02alt <garnold02alt@protonmail.com>
2022-02-11 10:45:35 +0100

    Added elements

--- editor/packages/app/src/logic/element.rs
+++ editor/packages/app/src/logic/element.rs
@@ -1,6 +1,29 @@
+use asset::TextureID;
+use cgmath::Vector3;
+
+use crate::graphics::SolidMesh;
+
 pub enum ElementKind {
     Solid,
     Face,
     Point,
     Prop,
+}
+
+pub struct Solid {
+    faces: [Face; 6],
+    points: [Point; 8],
+    selected: bool,
+    mesh: SolidMesh,
+}
+
+pub struct Face {
+    texture: TextureID,
+    indices: [usize; 4],
+    selected: bool,
+}
+
+pub struct Point {
+    position: Vector3<i32>,
+    selected: bool,
 }
