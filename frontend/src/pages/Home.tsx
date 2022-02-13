cb39ff75f6bce615326712123e357171f7ad358f
YertofeX <szabodaniel35@gmail.com>
2022-02-13 15:16:08 +0100

    Footer

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import HomeHeader from "../components/home-components/HomeHeader";
 import HomeContent from "../components/home-components/HomeContent";
+import Footer from "../components/general-components/Footer";
 
 export default function Home() {
   return (
     <>
       <HomeHeader />
       <HomeContent />
+      <Footer />
     </>
   );
 }
