1fd8b0832cb5b99694e5c07456ffd48c1b33a5b8
YertofeX <szabodaniel35@gmail.com>
2022-02-14 10:01:04 +0100

    Login page

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "../components/general-components/Footer";
+
+import LoginForm from "../components/login-components/LoginForm";
+
+export default function Login() {
+  return (
+    <>
+      <LoginForm />
+      <Footer />
+    </>
+  );
+}
