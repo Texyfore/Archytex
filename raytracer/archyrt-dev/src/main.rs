c224cb434c2e38830ac98785c7c5fe8de5ddfc6f
bela333 <b3kstudio@gmail.com>
2022-02-22 13:01:58 +0100

    Set dev up for basic rendering tasks

--- raytracer/archyrt-dev/src/main.rs
+++ raytracer/archyrt-dev/src/main.rs
@@ -1,12 +1,13 @@
-
-
+use archyrt_core::api::camera::Camera;
 use archyrt_core::api::fragment_render::{FragmentContext, FragmentRender};
 
 use archyrt_core::cameras::jitter::JitterCamera;
+use archyrt_core::collector::image_collector::ImageCollector;
 use archyrt_core::collector::raw_collector::RawCollector;
 use archyrt_core::intersectables::bvh::BVH;
 use archyrt_core::intersectables::sphere::Sphere;
 use archyrt_core::loaders::ascn::{amdl_textures, ASCNLoader};
+use archyrt_core::renderers::basic_renderer::BasicRenderer;
 use archyrt_core::renderers::path_tracer::{Material, PathTracer};
 use archyrt_core::renderers::sampling::SamplingRenderer;
 use archyrt_core::renderers::solid_renderers::albedo::AlbedoRenderer;
@@ -17,7 +18,8 @@ use archyrt_core::utilities::math::{Vec2, Vector};
 use archyrt_core::utilities::ray::{Intersectable, Ray};
 use archyrt_core::vector;
 use archyrt_core::{
-    api::fragment_collector::FragmentCollector, loaders::Loader, textures::TextureID, utilities::math::Vec3,
+    api::fragment_collector::FragmentCollector, loaders::Loader, textures::TextureID,
+    utilities::math::Vec3,
 };
 use image::{Rgb, RgbImage};
 use rayon::prelude::*;
@@ -37,27 +39,11 @@ impl<Renderer: FragmentRender + Sync + Send> FragmentRender for ParallelSampling
     }
 }
 
-
-fn main() {
-    let w = 1920 / 2;
-    let h = 1080 / 2;
-    
-    //Loading textures and skybox
-    println!("Load file");
-    let mut repo = TextureRepository::new();
-    amdl_textures::load_into(&mut repo, "../assets").unwrap();
+fn render_pathtraced<O: Intersectable+Sync, C: Camera+Sync>(object: O, camera: C, mut repo: TextureRepository, w: usize, h: usize) -> image::ImageBuffer<Rgb<u8>, Vec<u8>> {
+    let aa_camera = JitterCamera::new(&camera, w, h); //Camera used for anti-aliasing
     let skybox_id = TextureID::new(&"skybox");
     texture_repo::exr::load_into(&mut repo, "../assets", &[(skybox_id, "skybox.exr")]).unwrap();
     let skybox = Some(skybox_id);
-
-    //Load model
-    let loader = ASCNLoader::from_path("../assets/house_outside.ascn").unwrap();
-    let camera = loader.get_camera();
-    let aa_camera = JitterCamera::new(camera, w, h); //Camera used for anti-aliasing
-    let object = loader.get_triangles();
-    let object = BVH::from_triangles(object).unwrap(); //Generate acceleration structure
-
-    println!("Render");
     //Set renderers up
     let pathtracer = PathTracer {
         skybox,
@@ -75,15 +61,15 @@ fn main() {
         camera: &aa_camera,
     };
     //Make sure albedo is anti-aliased
-    let albedo = ParallelSamplingRenderer{
+    let albedo = ParallelSamplingRenderer {
         inner: albedo,
-        samples: 5
+        samples: 5,
     };
     let normal = NormalRenderer {
         object: &object,
         camera: &camera,
     };
-    
+
     //Collect images to arrays
     println!("Rendering image");
     let collector = RawCollector {};
@@ -93,7 +79,10 @@ fn main() {
 
     //Using OIDN for denoising
     println!("Denoising");
-    let mut output: Vec<f32> = (0..pathtracer_image.len()).into_iter().map(|_| 0f32).collect();
+    let mut output: Vec<f32> = (0..pathtracer_image.len())
+        .into_iter()
+        .map(|_| 0f32)
+        .collect();
     let device = oidn::Device::new();
     oidn::RayTracing::new(&device)
         .srgb(false)
@@ -105,18 +94,21 @@ fn main() {
         .unwrap();
     //Collect OIDN image
     let mut image = RgbImage::new(w as u32, h as u32);
-    
-    let output: Vec<Vec3> = output.chunks(3).map(|v|{
-        let [r, g, b]: [f32;3] = v.try_into().unwrap();
-        vector![r as f64, g as f64, b as f64]
-    }).collect();
+
+    let output: Vec<Vec3> = output
+        .chunks(3)
+        .map(|v| {
+            let [r, g, b]: [f32; 3] = v.try_into().unwrap();
+            vector![r as f64, g as f64, b as f64]
+        })
+        .collect();
     for (x, y, color) in image.enumerate_pixels_mut() {
         let index = y as usize * w + x as usize;
         let c = output[index];
 
         let c = tonemap_fragment(c);
 
-        let c = c*255.;
+        let c = c * 255.;
         let r = c.x();
         let g = c.y();
         let b = c.z();
@@ -128,5 +120,64 @@ fn main() {
         let b = b as u8;
         *color = Rgb([r, g, b]);
     }
+    image
+}
+
+fn render_albedo<O: Intersectable, C: Camera>(object: O, camera: C, mut repo: TextureRepository, w: usize, h: usize) -> image::ImageBuffer<Rgb<u8>, Vec<u8>>{
+    let renderer = BasicRenderer{
+        camera,
+        object,
+        lamp: vector![3.0, 2.0, 0.0]
+    };
+    let collector = RawCollector{};
+    let mut image = RgbImage::new(w as u32, h as u32);
+    let output = collector.collect(renderer, &repo, w, h);
+
+    let output: Vec<Vec3> = output
+        .chunks(3)
+        .map(|v| {
+            let [r, g, b]: [f32; 3] = v.try_into().unwrap();
+            vector![r as f64, g as f64, b as f64]
+        })
+        .collect();
+    for (x, y, color) in image.enumerate_pixels_mut() {
+        let index = y as usize * w + x as usize;
+        let c = output[index];
+
+        let c = tonemap_fragment(c);
+
+        let c = c * 255.;
+        let r = c.x();
+        let g = c.y();
+        let b = c.z();
+        let r = r.clamp(0.0, 255.0);
+        let g = g.clamp(0.0, 255.0);
+        let b = b.clamp(0.0, 255.0);
+        let r = r as u8;
+        let g = g as u8;
+        let b = b as u8;
+        *color = Rgb([r, g, b]);
+    }
+    image
+}
+
+fn main() {
+    let w = 512;
+    let h = 512;
+
+    //Loading textures and skybox
+    println!("Load file");
+    let mut repo = TextureRepository::new();
+    amdl_textures::load_into(&mut repo, "../assets").unwrap();
+
+    //Load model
+    let loader = ASCNLoader::from_path("../assets/house_inside.ascn").unwrap();
+    let camera = loader.get_camera();
+    let object = loader.get_triangles();
+    let object = BVH::from_triangles(object).unwrap(); //Generate acceleration structure
+
+    println!("Render");
+    //let image = render_pathtraced(object, camera, repo, w, h);
+    let image = render_albedo(object, camera, repo, w, h);
     image.save("image.png").unwrap();
 }
