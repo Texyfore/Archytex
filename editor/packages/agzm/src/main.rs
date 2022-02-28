722d33e6b37c5dcce6a344f850763447a0f29c91
garnold02alt <garnold02alt@protonmail.com>
2022-02-28 10:55:59 +0100

    Still fixing assets

--- editor/packages/agzm/src/main.rs
+++ editor/packages/agzm/src/main.rs
@@ -15,40 +15,42 @@ fn main() {
     let outdir = matches.value_of("out").unwrap();
 
     for entry in read_dir(indir).unwrap().flatten() {
-        let (document, buffers, _) = gltf::import(entry.path()).unwrap();
-        let mesh = document.meshes().next().unwrap();
-        let primitive = mesh.primitives().next().unwrap();
-        let reader = primitive.reader(|buf| Some(&buffers[buf.index()]));
-        let vertices = reader
-            .read_positions()
-            .unwrap()
-            .map(|p| GizmoVertex { position: p.into() })
+        if entry.file_name().to_str().unwrap().contains("gltf") {
+            let (document, buffers, _) = gltf::import(entry.path()).unwrap();
+            let mesh = document.meshes().next().unwrap();
+            let primitive = mesh.primitives().next().unwrap();
+            let reader = primitive.reader(|buf| Some(&buffers[buf.index()]));
+            let vertices = reader
+                .read_positions()
+                .unwrap()
+                .map(|p| GizmoVertex { position: p.into() })
+                .collect::<Vec<_>>();
+
+            let indices = if let ReadIndices::U16(indices) = reader.read_indices().unwrap() {
+                indices
+            } else {
+                panic!("Bad indices")
+            }
             .collect::<Vec<_>>();
 
-        let indices = if let ReadIndices::U16(indices) = reader.read_indices().unwrap() {
-            indices
-        } else {
-            panic!("Bad indices")
+            let triangles = indices
+                .chunks_exact(3)
+                .map(|w| w.try_into().unwrap())
+                .collect();
+
+            let out = Gizmo {
+                vertices,
+                triangles,
+            }
+            .encode()
+            .unwrap();
+
+            let path = format!(
+                "{}/{}",
+                outdir,
+                entry.file_name().to_str().unwrap().replace("gltf", "agzm")
+            );
+            write(path, &out).unwrap();
         }
-        .collect::<Vec<_>>();
-
-        let triangles = indices
-            .chunks_exact(3)
-            .map(|w| w.try_into().unwrap())
-            .collect();
-
-        let out = Gizmo {
-            vertices,
-            triangles,
-        }
-        .encode()
-        .unwrap();
-
-        let path = format!(
-            "{}/{}",
-            outdir,
-            entry.file_name().to_str().unwrap().replace("gltf", "agzm")
-        );
-        write(path, &out).unwrap();
     }
 }
