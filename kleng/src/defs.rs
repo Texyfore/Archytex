217f173af545314ddec56dfd8ceaf646c39dcd06
garnold02alt <garnold02alt@protonmail.com>
2022-03-08 09:55:09 +0100

    Defs simplification

--- kleng/src/defs.rs
+++ kleng/src/defs.rs
@@ -5,21 +5,12 @@ use serde::{Deserialize, Serialize};
 use crate::{fsutil::CanonPath, require::Require};
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct TextureDef {
-    pub name: String,
-    pub source: String,
-    pub categories: Vec<String>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
 pub struct PropDef {
-    pub name: String,
-    pub source: String,
-    pub textures: HashMap<String, String>,
     pub categories: Vec<String>,
+    pub textures: Option<HashMap<String, String>>,
 }
 
-pub fn read(root: &Path) -> (Vec<TextureDef>, Vec<PropDef>) {
+pub fn read(root: &Path) -> (HashMap<String, Vec<String>>, HashMap<String, PropDef>) {
     let textures = {
         let path = CanonPath::new(root.join("textures/defs.json")).require();
         let file = File::open(path).require();
