a1009b54b82bd7c179d624c84107952b7d4fbcfd
garnold02alt <garnold02alt@protonmail.com>
2022-03-08 09:15:11 +0100

    kleng rewrite

--- kleng/src/fsutil.rs
+++ kleng/src/fsutil.rs
@@ -1,16 +1,31 @@
-use std::path::{Path, PathBuf};
+use std::{
+    error::Error,
+    path::{Path, PathBuf},
+};
+
+use thiserror::Error;
 
 pub struct CanonPath {
     inner: PathBuf,
 }
 
 impl CanonPath {
-    pub fn new<P>(path: P) -> Option<Self>
+    pub fn new<P>(path: P) -> Result<Self, CanonError>
     where
         P: AsRef<Path>,
     {
         let buf = path.as_ref().to_owned();
-        buf.canonicalize().ok().map(|inner| Self { inner })
+        let inner = match buf.canonicalize() {
+            Ok(ok) => ok,
+            Err(err) => {
+                return Err(CanonError {
+                    path: buf,
+                    source: Box::new(err),
+                })
+            }
+        };
+
+        Ok(Self { inner })
     }
 }
 
@@ -18,4 +33,13 @@ impl AsRef<Path> for CanonPath {
     fn as_ref(&self) -> &Path {
         self.inner.as_ref()
     }
+}
+
+#[derive(Debug, Error)]
+#[error("Failed to canonicalize path `{path}`")]
+pub struct CanonError {
+    path: PathBuf,
+
+    #[source]
+    source: Box<dyn Error>,
 }
--- kleng/src/main.rs
+++ kleng/src/main.rs
@@ -1,28 +1,10 @@
-mod compile;
-mod db;
-mod fsutil;
-mod model;
-mod props;
-mod report;
-mod textures;
-
 use clap::{Arg, ArgMatches, Command};
-use compile::compile;
-use props::enumerate_props;
 
-use crate::{props::parse_defs, textures::enumerate_textures};
+mod fsutil;
+mod require;
 
 fn main() {
-    let matches = cmd();
-    let root = matches.value_of("in").unwrap();
-    let textures = enumerate_textures(root);
-
-    let props = {
-        let defs = parse_defs(root);
-        enumerate_props(root, defs, &textures)
-    };
-
-    compile(root, textures, props);
+    let _matches = cmd();
 }
 
 fn cmd() -> ArgMatches {
