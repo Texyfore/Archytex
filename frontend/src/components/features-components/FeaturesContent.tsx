cdf6dc9ac9d83b29dc8d8c71d9e68650e9d25780
YertofeX <szabodaniel35@gmail.com>
2022-03-11 15:14:39 +0100

    features page

--- frontend/src/components/features-components/FeaturesContent.tsx
+++ frontend/src/components/features-components/FeaturesContent.tsx
@@ -1,5 +1,44 @@
 import React from "react";
 
+import { useTranslation } from "react-i18next";
+
+import HomeSection from "../home-components/HomeSection";
+
+import section1Image from "../../img/illustrations/projects.png";
+import section2Image from "../../img/illustrations/editor.png";
+import section3Image from "../../img/illustrations/rendered.png";
+
 export default function FeaturesContent() {
-  return <div>FeaturesContent</div>;
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <HomeSection
+        title={t("features_section1_title")}
+        subtitle={t("features_section1_subtitle")}
+        paragraph={t("features_section1_paragraph")}
+        imageSrc={section1Image}
+        imageAlt='Projects'
+        flipped
+        hasDecoration
+      />
+      <HomeSection
+        title={t("features_section2_title")}
+        subtitle={t("features_section2_subtitle")}
+        paragraph={t("features_section2_paragraph")}
+        imageSrc={section2Image}
+        imageAlt='Editor'
+        hasDecoration
+      />
+      <HomeSection
+        title={t("features_section3_title")}
+        subtitle={t("features_section3_subtitle")}
+        paragraph={t("features_section3_paragraph")}
+        imageSrc={section3Image}
+        imageAlt='Renders'
+        flipped
+        hasDecoration
+      />
+    </>
+  );
 }
