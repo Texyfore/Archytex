e75522c6def7fff0f67fa0827fe011caa6df41b7
YertofeX <szabodaniel35@gmail.com>
2022-03-15 21:53:01 +0100

    Frontend documentation

--- frontend/src/services/colorMode.tsx
+++ frontend/src/services/colorMode.tsx
@@ -36,8 +36,8 @@ function GetTheme(mode: ColorMode): any {
             primary: mode === ColorMode.Dark ? "#F4F4F4" : "#1B1B1A",
           },
           background: {
-            default: mode === ColorMode.Dark ? "#1B1B1A" : "#F4F4F4",
-            paper: mode === ColorMode.Dark ? "#1B1B1A" : "#F4F4F4",
+            default: mode === ColorMode.Dark ? "#1B1B1A" : "#FFFFFF",
+            paper: mode === ColorMode.Dark ? "#1B1B1A" : "#FFFFFF",
           },
           divider: mode === ColorMode.Dark ? "#1F1F1F" : "#EBE7EC",
         },
