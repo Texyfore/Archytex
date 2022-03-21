43d2ed679a3304918ed9b8c078d59ef97db217e3
YertofeX <szabodaniel35@gmail.com>
2022-03-21 08:39:10 +0100

    Use skeleton while render is not finished

--- frontend/src/components/dashboard-components/render-card-components/RenderCard.tsx
+++ frontend/src/components/dashboard-components/render-card-components/RenderCard.tsx
@@ -7,6 +7,7 @@ import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
+import Skeleton from "@mui/material/Skeleton";
 
 import CircularProgressWithLabel from "../../general-components/CircularProgressWithLabel";
 import EnlargeImageModal from "./EnlargeImageModal";
@@ -39,40 +40,49 @@ export default function RenderCard({ render, project }: RenderCardProps) {
           disabled={render.status < 1}
           onClick={() => handleOpenEnlargeRenderModal(render)}
         >
-          <CardMedia
-            component='img'
-            sx={{
-              height: { xs: "150px", sm: "200px", md: "250px" },
-            }}
-            image={`${Environment.base_url}render/${render.icon}`}
-            alt='archytex_render'
-          />
+          {render.status < 1 ? (
+            <Skeleton
+              variant='rectangular'
+              animation='wave'
+              sx={{ height: { xs: "150px", sm: "200px", md: "250px" } }}
+            />
+          ) : (
+            <CardMedia
+              component='img'
+              sx={{
+                height: { xs: "150px", sm: "200px", md: "250px" },
+              }}
+              image={
+                render.status < 1
+                  ? ""
+                  : `${Environment.base_url}render/${render.icon}`
+              }
+              alt='archytex_render'
+            />
+          )}
           {/* Image overlay for progress information */}
-          <Box
-            position='relative'
-            width='100%'
-            height={0}
-            display={render.status < 100 ? "block" : "none"}
-          >
-            <Box
-              position='absolute'
-              top={{ xs: "-150px", sm: "-200px", md: "-250px" }}
-              height={{ xs: "150px", sm: "200px", md: "250px" }}
-              width='100%'
-              display='flex'
-              justifyContent='center'
-              alignItems='center'
-              bgcolor='rgba(0, 0, 0, 0.7)'
-            >
-              <Box>
-                <CircularProgressWithLabel
-                  size={80}
-                  thickness={1}
-                  value={render.status * 100}
-                />
+          {render.status < 1 && (
+            <Box position='relative' width='100%' height={0}>
+              <Box
+                position='absolute'
+                top={{ xs: "-150px", sm: "-200px", md: "-250px" }}
+                height={{ xs: "150px", sm: "200px", md: "250px" }}
+                width='100%'
+                display='flex'
+                justifyContent='center'
+                alignItems='center'
+                bgcolor='rgba(0, 0, 0, 0.7)'
+              >
+                <Box>
+                  <CircularProgressWithLabel
+                    size={80}
+                    thickness={1}
+                    value={Math.floor(render.status) * 100}
+                  />
+                </Box>
               </Box>
             </Box>
-          </Box>
+          )}
           <Tooltip title={render.name} placement='top'>
             <CardContent sx={{ maxHeight: "100px" }}>
               <Typography variant='h6' component='div' noWrap>
